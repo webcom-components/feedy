6f2d1a4588228df5c4e1af32e606da3ee4dde9ae
Paul Musso <paul.musso@orange.com>
2016-03-11 11:28:41 +0100

    chore(CI): prepare for travis and semantic-release

--- deploy-gh-pages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd dist
-git init
-git config user.name "PaulMusso"
-git config user.email "paul.musso@orange.com"
-git add .
-git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
