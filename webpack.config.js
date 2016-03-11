78398bc64d421a7e65033be7bab5eaa431e726c6
Paul Musso <paul.musso@orange.com>
2016-03-11 11:32:18 +0100

    chore: missing dependency in webpack conf ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const process = require('process');
